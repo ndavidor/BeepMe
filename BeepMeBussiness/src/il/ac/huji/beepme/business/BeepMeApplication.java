38c20644b7772457018ab9d6844a7b638a7f994e
Ofir Marcus <ofirule@gmail.com>
2013-10-02 21:59:10 +0300

    minor change in documentation plus adding apks

--- BeepMeBussiness/src/il/ac/huji/beepme/business/BeepMeApplication.java
+++ BeepMeBussiness/src/il/ac/huji/beepme/business/BeepMeApplication.java
@@ -4,9 +4,6 @@ import il.ac.huji.beepme.db.EmployeeAdapter;
 import il.ac.huji.beepme.db.Queue;
 import il.ac.huji.beepme.db.QueueAdapter;
 
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 import android.content.pm.ApplicationInfo;
@@ -17,29 +14,8 @@ import android.os.Bundle;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
-import il.ac.huji.beepme.business.R;
 
-// acralyzer URL: https://rey5137.cloudant.com/acralyzer/_design/acralyzer/index.html
-@ReportsCrashes(
-        formKey = "",
-        formUri = "https://rey5137.cloudant.com/acra-beepme/_design/acra-storage/_update/report",
-        reportType = org.acra.sender.HttpSender.Type.JSON,
-        httpMethod = org.acra.sender.HttpSender.Method.PUT,
-        formUriBasicAuthLogin="innothaldnturthersurside",
-        formUriBasicAuthPassword="fO6Rf1wJqppj5CJQtYaKV5pq",
-        
-        mode = ReportingInteractionMode.NOTIFICATION,
-        resToastText = R.string.crash_toast_text, 
-        resNotifTickerText = R.string.crash_notif_ticker_text,
-        resNotifTitle = R.string.crash_notif_title,
-        resNotifText = R.string.crash_notif_text,
-        resNotifIcon = android.R.drawable.stat_notify_error, 
-        resDialogText = R.string.crash_dialog_text,
-        resDialogIcon = android.R.drawable.ic_dialog_info,
-        resDialogTitle = R.string.crash_dialog_title,
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt,
-        resDialogOkToast = R.string.crash_dialog_ok_toast      
-)
+
 public class BeepMeApplication extends Application {
 
 	public static QueueAdapter adapter_queue = new QueueAdapter();
@@ -56,7 +32,6 @@ public class BeepMeApplication extends Application {
 		
 	public void onCreate(){
 		super.onCreate();
-		ACRA.init(this);
 		adapter_queue.setContext(getApplicationContext());
 		adapter_employee.setContext(getApplicationContext());
 		
